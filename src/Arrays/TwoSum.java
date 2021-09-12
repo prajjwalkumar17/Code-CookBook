0fe6000df7258704e1501abac202eed9ed7328c6
prajjwalkumar17 <talk2prajjwal@gmail.com>
2021-09-12 22:49:27 +0530

    Two sum completed

--- src/Arrays/TwoSum.java
+++ src/Arrays/TwoSum.java
@@ -14,6 +14,7 @@ public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         int[] x=new int[2];
         for(int i=0;i<nums.length;i++){
+
             for(int j=1;j<nums.length;j++){
                 if(j>i) {
                     int p;
